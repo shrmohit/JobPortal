edaa3a374a2ccbc142182239ff0ce7810c05df12
mohit sharma <mohitsharma7258@gmail.com>
2025-05-04 02:00:20 +0530

    four models

---
+++ backend/models/user.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNuber: {
+      type: Number,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Student", "recruiter"],
+      default: "Student",
+    },
+    profile: {
+      type: String,
+      bio: { type: String },
+      skills: [{ type: String }],
+      resume: { type: String },
+      resumeOriginalName: { type: String },
+      company: { type: mongoose.Schema.Types.ObjectId, ref: "company" },
+      profilePhoto: { type: String, default: "" },
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
