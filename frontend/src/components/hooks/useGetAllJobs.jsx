e4d5baa0504bee6abc0c6c24b8bc65845c2a36f7
mohit sharma <mohitsharma7258@gmail.com>
2025-05-08 13:40:07 +0530

    usegetalljob -> custom hooks

---
+++ frontend/src/components/hooks/useGetAllJobs.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { JOB_API_END_POINT } from "../../utils/constant";
+import { useDispatch } from "react-redux";
+import { setAllJobs } from "../../redux/jobSlice";
+import axios from "axios";
+
+const useGetAllJobs = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchAllJobs = async () => {
+      try {
+        const res = await axios.get(`${JOB_API_END_POINT}/get`, {
+          withCredentials: true,
+        });
+        if (res.data.success) {
+          dispatch(setAllJobs(res.data.jobs));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAllJobs();
+  }, []);
+};
+
+export default useGetAllJobs;
