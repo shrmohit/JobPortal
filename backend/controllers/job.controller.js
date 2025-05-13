5e0799ee508ae0ef80204de711cf163ad674fbb2
mohit sharma <mohitsharma7258@gmail.com>
2025-05-13 22:00:47 +0530

    admin job table

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -124,7 +124,11 @@ export const getJobById = async (req, res) => {
 export const getAdminJobs = async (req, res) => {
   try {
     const adminId = req.id;
-    const jobs = await Job.find({ created_by: adminId });
+    console.log("Current user ID:", req.id);
+    const jobs = await Job.find({ created_by: adminId }).populate({
+      path: "company",
+    });
+    console.log("Matching jobs:", jobs);
     if (!jobs) {
       return res.status(404).json({
         message: "Jobs not found",
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import JobDescription from "./components/JobDescription";
 import Companies from "./components/admin/Companies";
 import CompanyCreate from "./components/admin/CompanyCreate";
 import CompanySetup from "./components/admin/CompanySetup";
+import AdminJobs from "./components/admin/AdminJobs";
+import PostJob from "./components/admin/PostJob";
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -52,6 +54,14 @@ function App() {
     {
       path: "/admin/companies/:id",
       element: <CompanySetup />,
+    },
+    {
+      path: "/admin/jobs",
+      element: <AdminJobs />,
+    },
+    {
+      path: "/admin/job/create",
+      element: <PostJob />,
     },
   ]);
   return (
