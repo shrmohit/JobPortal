8ee8dd8bd17edd6a25d0212edd86d3c85489df46
mohit sharma <mohitsharma7258@gmail.com>
2025-05-14 23:02:41 +0530

    search

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import { Button } from "./ui/button";
 import { Search } from "lucide-react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchedQuery } from "../redux/JobSlice";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const [query, setQuery] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const searchJobHandler = () => {
+    dispatch(setSearchedQuery(query));
+    navigate("/browse");
+  };
   return (
     <div className="text-center">
       <div className="flex flex-col gap-5 my-10">
@@ -20,10 +32,15 @@ const HeroSection = () => {
         <div className="flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto">
           <input
             type="text"
+            value={query}
             placeholder="Find your dream jobs"
             className="outline-none border-none w-full"
+            onChange={(e) => setQuery(e.target.value)}
           />
-          <Button className="rounded-r-full bg-[#6A38C2]">
+          <Button
+            onClick={searchJobHandler}
+            className="rounded-r-full bg-[#6A38C2]"
+          >
             <Search className="h-5 w-5" />
           </Button>
         </div>
