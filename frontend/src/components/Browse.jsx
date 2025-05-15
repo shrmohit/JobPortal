1038bbf079b5b2a57c524592d061934ca9d71aa3
mohit sharma <mohitsharma7258@gmail.com>
2025-05-15 12:52:59 +0530

    jobSlice

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import Navbar from "./shared/Navbar";
 import Job from "./Job";
 import { useDispatch, useSelector } from "react-redux";
-import { setSearchedQuery } from "../redux/JobSlice";
+import { setSearchedQuery } from "../redux/jobSlice";
 import useGetAllJobs from "./hooks/useGetAllJobs";
 
 // const randomJobs = [1, 2, 45];
