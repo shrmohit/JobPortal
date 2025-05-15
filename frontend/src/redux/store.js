9c3d5e1ae7b2ff24db676fbe97acef887998ba14
mohit sharma <mohitsharma7258@gmail.com>
2025-05-15 12:45:36 +0530

    store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
-import jobSlice from "./JobSlice";
+import jobSlice from "./jobSlice";
 import companySlice from "./companySlice";
 import applicationSlice from "./applicantionSlice";
 import {
