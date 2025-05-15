41d40a038d1f62c98b0d413b12322af7ac3ed680
mohit sharma <mohitsharma7258@gmail.com>
2025-05-15 13:10:30 +0530

    SLICE

--- frontend/src/components/hooks/useGetAllAdminJobs.jsx
+++ frontend/src/components/hooks/useGetAllAdminJobs.jsx
@@ -1,4 +1,4 @@
-import { setAllAdminJobs } from "../../redux/JobSlice";
+import { setAllAdminJobs } from "../../redux/jobSlice";
 import { JOB_API_END_POINT } from "../../utils/constant";
 import axios from "axios";
 import { useEffect } from "react";
