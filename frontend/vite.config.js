87379f511eb01ec384717c51be062cb0b932b674
mohit sharma <mohitsharma7258@gmail.com>
2025-06-11 11:41:27 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,7 +10,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../backend/frontend/dist', // 👈 sends build to backend
-    emptyOutDir: true, // Clears the folder before building
+    outDir: '../backend/frontend/dist', // ✅ output directly inside backend
+    emptyOutDir: true,
   },
 });
