3b9a889ef2b487f809b3534576d437618e2611d2
mohit sharma <mohitsharma7258@gmail.com>
2025-07-28 18:22:18 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-
-  ],
+  origin:"http://localhost:5173/",
   credentials: true,
 }));
 
